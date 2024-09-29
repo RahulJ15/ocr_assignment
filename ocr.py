0f811075ea535a04fc9d4692a320e0df9f055df5
Rahul Jiandani <115068343+RahulJ15@users.noreply.github.com>
2024-09-29 20:21:54 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -3,11 +3,12 @@ import easyocr
 from PIL import Image
 import io
 import numpy as np
+from langdetect import detect  
 
 class ImageOCR:
     def __init__(self, image_path):
         self.image_path = image_path
-        self.image_data = None  # To store image data for in-memory processing
+        self.image_data = None 
 
     def convert_to_jpeg(self):
         img = Image.open(self.image_path)
@@ -15,7 +16,7 @@ class ImageOCR:
         if img.mode != 'RGB':
             img = img.convert('RGB')
         
-        # Save the image as JPEG in memory
+       
         jpeg_image_data = io.BytesIO()
         img.save(jpeg_image_data, format='JPEG')
         jpeg_image_data.seek(0)
@@ -23,19 +24,43 @@ class ImageOCR:
 
     def perform_ocr(self):
         if self.image_data:
-            # Load the image data from BytesIO for OCR processing
             img = Image.open(self.image_data)
             img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
         else:
             img_cv = cv2.imread(self.image_path)
         
         # Initialize EasyOCR reader for both Hindi and English languages
-        reader = easyocr.Reader(['hi', 'en'], gpu=True)  # Add 'hi' for Hindi
-        result = reader.readtext(img_cv)
-        extracted_text = " ".join([entry[1] for entry in result])
-        return extracted_text
+        reader = easyocr.Reader(['hi', 'en'], gpu=True)
+        result = reader.readtext(img_cv, detail=1)  
+        
+        extracted_text = []
+        last_y = 0
+        paragraph = []
+        detected_languages = set()  # To store all detected languages
 
-# Example usage (optional):
+        for entry in result:
+            bbox, text, _ = entry
+            top_left_y = bbox[0][1]
+            try:
+                detected_language = detect(text)
+                detected_languages.add(detected_language)  # Add language to the set
+            except:
+                detected_language = "unknown"
+            
+            if top_left_y - last_y > 40:  
+                if paragraph:
+                    extracted_text.append(" ".join(paragraph))
+                paragraph = [text]
+            else:
+                paragraph.append(text)
+            
+            last_y = top_left_y
+        if paragraph:
+            extracted_text.append(" ".join(paragraph))
+        language_info = ", ".join([lang.upper() for lang in detected_languages])
+        final_text = "\n\n".join(extracted_text
+        return f"Detected Languages: {language_info}\n\n{final_text}"
+                                 
 if __name__ == "__main__":
     image_path = 'path_to_your_image_file.png'  # Replace with the path to your image
     ocr = ImageOCR(image_path)
